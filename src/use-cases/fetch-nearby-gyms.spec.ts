b4c1963462b8a44f2bfa3e8f1483a30d963936e1
Vitor Lobo Muniz <vitolrobinho@hotmail.com>
2023-04-24 23:10:16 -0300

    Study

---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,38 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchNearbyGymsUseCase
+
+describe('Fetch Nearby Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchNearbyGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -22.993008,
+      longitude: -43.9846142,
+    })
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      phone: null,
+      latitude: -22.093008,
+      longitude: -43.0846142,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -22.993008,
+      userLongitude: -43.9846142,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
+  })
+})
